dbd571bc0bd1411a427d4a8c2717bf26441e28d4
Kallurisravani12 <KalluriSravani12>
2023-04-14 13:53:01 +0200

    My first commit

--- main.js
+++ main.js
@@ -1,9 +1,60 @@
-var a= 20;
-if (a==10){
+//For normal if
+var a= 20, b=20;
+if (a==b){
 
-    console.log("a is equal to 10")
+    console.log("a is equal to b")
 }
-if (a!=10){
+if (a!=b){
 
-    console.log("a is not equal to 10")
+    console.log("a is not equal to b")
 }
+// for if else
+var a= 30, b= 30;
+if (a==b){
+
+    console.log("a is equal to b")
+}
+else{
+
+    console.log("a is not equal to b")
+}
+
+// for else if
+var a= 10, b= 20;
+if (a==b){
+
+    console.log("a is equal to b")
+}
+else if(a>b){
+
+  console.log("a is greater than b")
+}
+else if(a <b){
+
+    console.log("a is less than to b")
+  }
+  else{
+
+    console.log("a is not equal to b")
+  }
+
+// for nested if
+
+
+  var a= 5, b= 10;
+if (a>=b){
+    if(a==b){
+
+    console.log("a is equal to b")
+}
+    else
+    {
+
+    console.log("a is greater than b")
+    }
+    
+  }
+else{
+
+    console.log("a is less than b")
+    }
